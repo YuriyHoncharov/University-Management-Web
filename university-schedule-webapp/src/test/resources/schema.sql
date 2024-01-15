d50362f3ba2d5d85f0b5baf0a4b0f37d4c8d2fcb
Yuriy Honcharov <yuriyhoncharov@gmail.com>
2024-01-15 23:49:47 +0100

    15.01.2024
    1. Last changes in sql query

--- university-schedule-webapp/src/test/resources/schema.sql
+++ university-schedule-webapp/src/test/resources/schema.sql
@@ -1,4 +1,9 @@
-CREATE TABLE IF NOT EXISTS Users {
+CREATE TABLE IF NOT EXISTS Grades (
+id SERIAL PRIMARY KEY,
+value VARCHAR(5) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Users (
 id SERIAL PRIMARY KEY,
 login TEXT NOT NULL,
 password TEXT NOT NULL,
@@ -8,41 +13,37 @@ roleId INT,
 gradeId INT,
 FOREIGN KEY (gradeId) REFERENCES Grades (id),
 FOREIGN KEY (roleId) REFERENCES Roles(id)
-};
+);
 
-CREATE TABLE IF NOT EXISTS Roles {
+CREATE TABLE IF NOT EXISTS Roles (
 id INT PRIMARY KEY, 
 roleName TEXT NOT NULL UNIQUE
-};
+);
 
-CREATE TABLE IF NOT EXISTS Grades {
-id SERIAL PRIMARY KEY,
-value VARCHAR(5) NOT NULL
-};
 
 INSERT INTO Roles (id, roleName) VALUES (1, 'Admin');
 INSERT INTO Roles (id, roleName) VALUES (2, 'Student');
 INSERT INTO Roles (id, roleName) VALUES (3, 'Teacher');
 
-CREATE TABLE IF NOT EXISTS Subjects {
+CREATE TABLE IF NOT EXISTS Subjects (
 id PRIMARY KEY,
 name TEXT NOT NULL,
 description TEXT NOT NULL
-};
+);
 
-CREATE TABLE IF NOT EXISTS Auditoriums {
+CREATE TABLE IF NOT EXISTS Auditoriums (
 id PRIMARY KEY,
 name VARCHAR(5) NOT NULL
-};
+);
 
-CREATE TABLE IF NOT EXISTS Enrollments {
+CREATE TABLE IF NOT EXISTS Enrollments (
 userId INT,
 FOREIGN KEY (userId) REFERENCES Users (id),
 subjectId INT,
 FOREIGN KEY(subjectId) REFERENCES Subjects (id)
-};
+);
 
-CREATE TABLE IF NOT EXISTS Lessons {
+CREATE TABLE IF NOT EXISTS Lessons (
 id SERIAL PRIMARY KEY,
 subjectId INT NOT NULL,
 FOREIGN KEY (subjectId) REFERENCES Subjects (id),
@@ -53,4 +54,4 @@ auditoriumId INT NOT NULL,
 FOREIGN KEY (auditoriumId) REFERENCES Auditoriums (id),
 gradeId INT NOT NULL,
 FOREIGN KEY (gradeId) REFERENCES Grades (id)
-};
+);
